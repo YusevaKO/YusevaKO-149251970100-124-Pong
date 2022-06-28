375141925a5595cbda8e3f2d711865901dfb1991
YusevaKO <107090371+YusevaKO@users.noreply.github.com>
2022-06-28 16:56:35 +0700

    Features addition - Problem solved rev 1
    baru ada fitur tombol back to main menu, credit scene.

--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -6,25 +6,32 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject textDisplay;
+    public GameObject DisplayMisi;
+    public GameObject menu;
+    public GameObject DisplayCredit;
     public void PlayGame()
     {
         SceneManager.LoadScene("game");
     }
-    public void OpenAuthor()
+    public void Menucredit()
     {
+        menu.SetActive(false);
+        DisplayCredit.SetActive(true);
         Debug.Log("Created by Yuseva-149251970100-124");
     }
-    public void Misi()
+    public void backmenu1()
     {
-        string misi;
-        misi = "Dalam permainan ini kamu akan menggunakan \n"+
-            "tombol S dan W untuk menggerakan Padel Kanan ! \n" +
-            "tombol A dan Q untuk menggerakan Padel Kiri ! \n"
-
-            ;
-        Debug.Log(misi);
-        textDisplay.GetComponent<Text>().text = "" + misi;
+        DisplayCredit.SetActive(false);
+        menu.SetActive(true);
     }
-
+    public void MenuMisi()
+    {
+        menu.SetActive(false);
+        DisplayMisi.SetActive(true);
+    }
+    public void backmenu2()
+    {
+        DisplayMisi.SetActive(false);
+        menu.SetActive(true);
+    }
 }
